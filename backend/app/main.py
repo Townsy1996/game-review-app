7857ccf7931bf4b154b7275ee6c6d30c244064e0
Corey <corey_townsend@outlook.com>
2026-08-19 15:22:19 +0100

    Add review router to main, add versions to requirements

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from app import models
 from app.database import engine
-from app.routers import games, users
+from app.routers import games, users, reviews
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Game Vault API")
 app.include_router(games.router)
 app.include_router(users.router)
+app.include_router(reviews.router)
