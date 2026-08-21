88415a474d6bc69631212ea7a0cdfe66b773c2e8
Corey <corey_townsend@outlook.com>
2026-08-21 17:42:56 +0100

    Add user auth and create new review function

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -3,6 +3,14 @@ import jwt
 from fastapi.security import OAuth2PasswordBearer
 from pwdlib import PasswordHash
 from app.config import settings
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+import app.models
+from app.config import settings
+from app.database import get_db
+from uuid import UUID
 
 password_hash = PasswordHash.recommended()
 
@@ -47,3 +55,42 @@ def verify_access_token(token: str) -> str | None:
         return None
     else:
         return payload.get("sub")
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db: Annotated[AsyncSession, Depends(get_db)],
+) -> app.models.User:
+    user_id = verify_access_token(token)
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    try:
+        user_id_uuid = UUID(user_id)
+    except (TypeError, ValueError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    result = await db.execute(
+        select(app.models.User).where(app.models.User.id == user_id_uuid)
+    )
+
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return user
+
+
+CurrentUser = Annotated[app.models.User, Depends(get_current_user)]
--- backend/app/routers/reviews.py
+++ backend/app/routers/reviews.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.database import get_db
-from app.models import Review
+from app.models import Review, Game
 from app.schemas import ReviewCreate, ReviewResponse, ReviewUpdate
 from uuid import UUID
 from sqlalchemy.orm import joinedload
+from app.auth import CurrentUser
 
 router = APIRouter(tags=["Reviews"])
 
@@ -38,7 +39,44 @@ async def get_reviews_from_user(
     user_id: UUID, db: Annotated[AsyncSession, Depends(get_db)]
 ):
     result = await db.execute(
-        select(Review).options(joinedload(Review.game)).where(Review.user_id == user_id)
+        select(Review)
+        .options(joinedload(Review.game), joinedload(Review.user))
+        .where(Review.user_id == user_id)
     )
     reviews = result.scalars().all()
     return reviews
+
+
+@router.post("/", response_model=ReviewResponse, status_code=status.HTTP_201_CREATED)
+async def create_review(
+    review: ReviewCreate,
+    current_user: CurrentUser,
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    result = await db.execute(select(Game).where(Game.id == review.game_id))
+    game = result.scalars().first()
+    if not game:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Game not found"
+        )
+
+    result = await db.execute(
+        select(Review).where(
+            Review.game_id == review.game_id, Review.user_id == current_user.id
+        )
+    )
+
+    existing_review = result.scalars().first()
+    if existing_review:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="A review for this game already exists by the current user",
+        )
+    new_review = Review(**review.model_dump(), user_id=current_user.id)
+    db.add(new_review)
+    await db.commit()
+    await db.refresh(new_review)
+    new_review.user = current_user
+    new_review.game = game
+
+    return new_review
